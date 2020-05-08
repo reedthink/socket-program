34f77d0546596024214e7b839a1830d8aa82ca0a
DevOnLinux <reedthink@qq.com>
2020-05-08 22:54:18 +0800

    添加回声服务器

---
+++ echo_server.c
@@ -0,0 +1,67 @@
+//隐患：tcp传输不存在数据边界，可能出现内容未传输完毕，客户端就read
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define BUF_SIZE 1024 //设置缓存大小
+void error_handling(char *message);
+
+int main(int argc, char *argv[])
+{
+	int serv_sock, clnt_sock;
+	char message[BUF_SIZE];
+	int str_len, i;
+	
+	struct sockaddr_in serv_adr;
+	struct sockaddr_in clnt_adr;
+	socklen_t clnt_adr_sz;
+	
+	if(argc!=2) {
+		printf("Usage : %s <port>\n", argv[0]);
+		exit(1);
+	}
+	
+	serv_sock=socket(PF_INET, SOCK_STREAM, 0);   
+	if(serv_sock==-1)
+		error_handling("socket() error");
+	
+	memset(&serv_adr, 0, sizeof(serv_adr));
+	serv_adr.sin_family=AF_INET;
+	serv_adr.sin_addr.s_addr=htonl(INADDR_ANY);
+	serv_adr.sin_port=htons(atoi(argv[1]));
+
+	if(bind(serv_sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr))==-1)
+		error_handling("bind() error");
+	
+	if(listen(serv_sock, 5)==-1)
+		error_handling("listen() error");
+	
+	clnt_adr_sz=sizeof(clnt_adr);
+	//下方实现了迭代方式为5个客户端提供服务。共调用5次accept函数
+	for(i=0; i<5; i++)
+	{
+		clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_adr, &clnt_adr_sz);
+		if(clnt_sock==-1)
+			error_handling("accept() error");
+		else
+			printf("Connected client %d \n", i+1);
+	
+		while((str_len=read(clnt_sock, message, BUF_SIZE))!=0)//回声代码
+			write(clnt_sock, message, str_len);
+
+		close(clnt_sock);
+	}
+
+	close(serv_sock);
+	return 0;
+}
+
+void error_handling(char *message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
